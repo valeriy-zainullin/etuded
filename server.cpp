b7334058929fd11b44cf7cc1f4850da6210e3346
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2024-11-27 02:24:05 +0300

    Start working on etude compiler interaction.

--- server.cpp
+++ server.cpp
@@ -5,18 +5,24 @@
 #include <condition_variable>
 #include <memory>
 
+// LibLsp.
 #include "LibLsp/lsp/ProtocolJsonHandler.h"
 #include "LibLsp/JsonRpc/stream.h"
 #include "LibLsp/JsonRpc/RemoteEndPoint.h"
 #include "LibLsp/JsonRpc/Endpoint.h"
 #include "LibLsp/JsonRpc/Condition.h"
-
 #include "LibLsp/lsp/general/initialize.h"
 #include "LibLsp/lsp/general/initialized.h"
 #include "LibLsp/lsp/general/exit.h"
 #include "LibLsp/lsp/textDocument/did_open.h"
+#include "LibLsp/lsp/textDocument/documentColor.h"
 #include "LibLsp/lsp/lsTextDocumentIdentifier.h" // Missing include inside of did_close.h, okay...
+#include "LibLsp/lsp/lsDocumentUri.h"
 #include "LibLsp/lsp/textDocument/did_close.h"
+
+// Etude compiler.
+#include "driver/compil_driver.hpp"
+#include "driver/module.hpp"
 
 class Logger final : public lsp::Log {
 public:
@@ -77,6 +83,7 @@ int main(int argc, char** argv) {
     fs::path compiler_path  = exec_dir / "etude" / "123";
     // TODO: figure out compiler path!
 
+    std::atomic<bool> initialized = false;
     std::atomic<bool> exiting = false;
 
     Logger logger;
@@ -87,11 +94,55 @@ int main(int argc, char** argv) {
     auto json_handler = std::make_shared<lsp::ProtocolJsonHandler>();
     RemoteEndPoint client_endpoint(json_handler, server_endpoint, logger);
 
-    client_endpoint.registerHandler([&](const td_initialize::request& init_parms) {
-        return td_initialize::response();
+    // https://github.com/kuafuwang/LspCpp/blob/e0b443d42e7d23638d727ac8ef6839b9e527bf0a/examples/StdIOServerExample.cpp#L57
+    client_endpoint.registerHandler([&](const td_initialize::request& request) {
+        td_initialize::response response;
+        
+        response.id = request.id;
+        response.result.capabilities = lsServerCapabilities {
+            .colorProvider = {{true, {}}}
+        };
+
+        return response;
+    });
+
+    std::map<std::string, Module> ast_cache;
+
+    client_endpoint.registerHandler([&](const td_documentColor::request& request) {
+        std::string doc_path = request.params.textDocument.uri.GetAbsolutePath();
+        std::vector<ColorInformation> coloring;
+
+        decltype(ast_cache)::iterator ast_it = ast_cache.find(doc_path);
+        if (ast_it != ast_cache.end()) {
+            colorFile(*ast_it);
+        } else {
+            // Компилятор на данный момент ищет файлы в рабочей директории.
+            //   В том числе, все импортируемые. Кроме стандартной библиотеки,
+            //   которую он найдет и так, если мы укажем переменную окружения.
+            //   Потому сменим рабочую директорию. Другие части нашего кода от
+            //   этого не зависят.
+            fs::path path(doc_path);
+            assert(path.is_absolute());
+
+            // https://stackoverflow.com/a/57096619
+            fs::current_path(path.parent_path());
+
+            std::string module_name = path.filename().replace_extension();
+            CompilationDriver driver(module_name);
+            if (!driver.Compile()) {
+
+            }
+
+        }
+
+        td_documentColor::response response;
+        response.id = request.id;
+        response.result = std::move(coloring);
+        return response;
     });
 
     client_endpoint.registerHandler([&](Notify_InitializedNotification::notify& notify) {
+        initialized.store(true);
     });
 
     client_endpoint.registerHandler([&](Notify_Exit::notify& notify) {
@@ -100,6 +151,9 @@ int main(int argc, char** argv) {
     });
 
     client_endpoint.registerHandler([&](Notify_TextDocumentDidOpen::notify& notify) {
+        if (!initialized) {
+            return;
+        }
         // Т.е. перед нами может быть даже не локальный файл, а на удаленном компьютере!
         logger.log(lsp::Log::Level::INFO, "opened file with uri " + notify.params.textDocument.uri.raw_uri_);
     });
